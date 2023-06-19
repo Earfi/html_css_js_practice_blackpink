8fb38705c8f1c0495b2c05cdabea2db24ed41ee4
Pichaya Chantrasriwong <pichaya8442@gmail.com>
2023-06-19 20:38:35 +0700

    30%

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar')
-
-let open_login = document.querySelector('.ri-user-fill')
-let close_login = document.querySelector('.ri-close-circle-line')
-let login = document.querySelector('.login')
+let header = document.querySelector('.header')
 
 menu.onclick = () => {
     menu.classList.toggle('active')
     navbar.classList.toggle('active')
 }
 
+
+let open_login = document.querySelector('.ri-user-fill')
+let close_login = document.querySelector('.ri-close-circle-line')
+
+// switch login register
+let containerLogin = document.querySelector('.container')
+let login_box = document.querySelector('.login-box')
+let go_login_link = document.querySelector('.go-login-link')
+let go_register_link = document.querySelector('.go-register-link')
+
+
 close_login.addEventListener('click',() => {
-   login.classList.remove('active')
+   containerLogin.classList.add('active-popup')
 })
 
 open_login.addEventListener('click',() => {
-   login.classList.add('active')
+   containerLogin.classList.remove('active-popup')
 })
+
+go_register_link.addEventListener('click',() => {
+   login_box.classList.add('active')
+})
+
+go_login_link.addEventListener('click',() => {
+   login_box.classList.remove('active')
+})
+
+const blurHeader = () => {
+   this.scrollY < 100 ? header.classList.remove('blur-header') : header.classList.add('blur-header');
+}
+
+window.addEventListener('scroll',blurHeader)
